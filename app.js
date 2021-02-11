cbec28611721ed914c7dab6eec4964c7847a72a7
satyadvaya <satyadvaya@gmail.com>
2021-02-10 22:29:57 -0800

    unable to get event listener to function properly but preserving progress

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const firstAddInput = document.getElementById('first-add-input');
+const secondAddInput = document.getElementById('second-add-input');
+const addButton = document.getElementById('add-button');
+const sumDisplay = document.getElementById('sum-display');
+
+addButton.addEventListener('click', () => {
+
+    const addTotal = Number(firstAddInput.value) + Number(secondAddInput.value);
+    sumDisplay.textContent = addTotal.value;
+
+});
