043a5fba1663c017d665b5ca6834a414432b0c08
satyadvaya <satyadvaya@gmail.com>
2021-02-11 18:24:21 -0800

    tests passed

--- app.js
+++ app.js
@@ -4,14 +4,23 @@
 
 // set event listeners to update state and DOM
 
+import { add, subtract, multiply, divide } from './calculations.js';
+
 const firstAddInput = document.getElementById('first-add-input');
 const secondAddInput = document.getElementById('second-add-input');
 const addButton = document.getElementById('add-button');
 const sumDisplay = document.getElementById('sum-display');
 
 addButton.addEventListener('click', () => {
-    const addTotal = Number(firstAddInput.value) + Number(secondAddInput.value);
-    sumDisplay.textContent = addTotal;
+    sumDisplay.textContent = add(Number(firstAddInput.value), Number(secondAddInput.value));
+
+    // const x = Number(firstAddInput.value);
+    // const y = Number(secondAddInput.value);
+    // const sum = add(x, y);
+    // sumDisplay.value = sum;
+
+    // const addTotal = Number(firstAddInput.value) + Number(secondAddInput.value);
+    // sumDisplay.textContent = addTotal;
 });
 
 const firstSubtractInput = document.getElementById('first-subtract-input');
@@ -20,8 +29,10 @@ const subtractButton = document.getElementById('subtract-button');
 const differenceDisplay = document.getElementById('difference-display');
 
 subtractButton.addEventListener('click', () => {
-    const subtractTotal = Number(firstSubtractInput.value) - Number(secondSubtractInput.value);
-    differenceDisplay.textContent = subtractTotal;
+    differenceDisplay.textContent = subtract(Number(firstSubtractInput.value), Number(secondSubtractInput.value));
+
+    // const subtractTotal = Number(firstSubtractInput.value) - Number(secondSubtractInput.value);
+    // differenceDisplay.textContent = subtractTotal;
 });
 
 const firstMultiplyInput = document.getElementById('first-multiply-input');
@@ -30,8 +41,10 @@ const multiplyButton = document.getElementById('multiply-button');
 const productDisplay = document.getElementById('product-display');
 
 multiplyButton.addEventListener('click', () => {
-    const multiplyTotal = Number(firstMultiplyInput.value) * Number(secondMultiplyInput.value);
-    productDisplay.textContent = multiplyTotal;
+    productDisplay.textContent = multiply(Number(firstMultiplyInput.value), Number(secondMultiplyInput.value));
+
+    // const multiplyTotal = Number(firstMultiplyInput.value) * Number(secondMultiplyInput.value);
+    // productDisplay.textContent = multiplyTotal;
 });
 
 const firstDivideInput = document.getElementById('first-divide-input');
@@ -40,6 +53,8 @@ const divideButton = document.getElementById('divide-button');
 const quotientDisplay = document.getElementById('quotient-display');
 
 divideButton.addEventListener('click', () => {
-    const divideTotal = Number(firstDivideInput.value) / Number(secondDivideInput.value);
-    quotientDisplay.textContent = divideTotal;
+    quotientDisplay.textContent = divide(Number(firstDivideInput.value), Number(secondDivideInput.value));
+
+    // const divideTotal = Number(firstDivideInput.value) / Number(secondDivideInput.value);
+    // quotientDisplay.textContent = divideTotal;
 });
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,82 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { add, subtract, multiply, divide } from '../calculations.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('time to test an add function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    // const expected = true;
+    const x = 1;
+    const y = 2;
+    const sum = 3;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    // const actual = false;
+    const result = add(x, y);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    // expect.equal(actual, expected);
+    expect.equal(result, sum);
+});
+
+test('time to test a subtract function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    // const expected = true;
+    const x = 3;
+    const y = 4;
+    const difference = -1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    // const actual = false;
+    const result = subtract(x, y);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    // expect.equal(actual, expected);
+    expect.equal(result, difference);
+});
+
+test('time to test a multiply function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    // const expected = true;
+    const x = 5;
+    const y = 6;
+    const product = 30;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    // const actual = false;
+    const result = multiply(x, y);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    // expect.equal(actual, expected);
+    expect.equal(result, product);
+});
+
+test('time to test a divide function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    // const expected = true;
+    const x = 7;
+    const y = 8;
+    const quotient = 0.875;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    // const actual = false;
+    const result = divide(x, y);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    // expect.equal(actual, expected);
+    expect.equal(result, quotient);
 });
